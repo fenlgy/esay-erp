078359dcf0688508672ffd12c74dbd4fe082ff54
fen <fen@fenth.com>
2024-01-14 23:11:07 +0800

    [更新] button click 事件加入防抖函数

--- src/components/basic/button/btn.vue
+++ src/components/basic/button/btn.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-button @click="handleClick" v-bind="$attrs">
+  <n-button @click="debounceClick" v-bind="$attrs">
     <template v-for="slot in Object.keys($slots)" v-slot:[slot]="scope">
       <slot :name="slot" v-bind="scope ?? {}" />
     </template>
@@ -8,19 +8,29 @@
 
 <script setup lang="ts">
 
+import { debounce } from "lodash";
+
 type Props = {
   to?: string | "back"
 }
 
 const props = withDefaults(defineProps<Props>(), {});
+const emit = defineEmits(["click"]);
 
 const router = useRouter();
-const handleClick = () => {
-  if (props.to === "back") {
-    router.go(-1);
+const handleClick = (e: Event) => {
+  if (props.to) {
+    if (props.to === "back") {
+      router.go(-1);
+    } else {
+      router.push(props.to);
+    }
+  } else {
+    emit("click", e);
   }
-  props.to && router.push(props.to);
 };
+const debounceClick = debounce(handleClick,200)
+
 </script>
 
 <style scoped>
