c3fec2028dc1f594bc3dd1afa5feb23613ac9578
fen <fen@fenth.com>
2024-01-14 22:07:47 +0800

    [更新] 重构表格内实用的便捷按钮，并增加新增按钮类型

--- src/components/basic/button/table-action-buttons.vue
+++ src/components/basic/button/table-action-buttons.vue
@@ -1,41 +1,49 @@
 <template>
-  <Btn v-bind="$attrs" :to="buttons.edit.to" quaternary size="small" @click="buttons.edit.onClick"
-       v-if="buttonsName.includes('edit')">
-    <n-icon>
-      <component :is="iconType['edit']" />
-    </n-icon>
-  </Btn>
-
-  <n-popconfirm v-if="buttonsName.includes('delete')"
-                negative-text="取消"
-                positive-text="确认"
-                @positive-click="handleDelete"
-  >
-    <template #trigger>
-      <Btn v-bind="$attrs" :to="buttons.delete.to" quaternary size="small" @click="buttons.delete.onClick">
-        <n-icon>
-          <component :is="iconType['delete']" />
-        </n-icon>
-      </Btn>
-    </template>
-    刀下留人～，死而不能复生！
-  </n-popconfirm>
+  <template v-for="btn in _buttons">
+    <n-popconfirm
+      v-if="btn.show !== false && (btn.type === 'delete' && btn.popConfirm !== false) "
+      negative-text="取消"
+      positive-text="确认"
+      @positive-click="handleDelete"
+    >
+      <template #trigger>
+        <Btn v-bind="$attrs" :to="btn.to" quaternary size="small" @click="btn.onClick">
+          <n-icon>
+            <component :is="iconType[<BtnType>btn.type]" />
+          </n-icon>
+        </Btn>
+      </template>
+      刀下留人～ ,死而不能复生!
+    </n-popconfirm>
+    <Btn v-bind="$attrs" :to="btn.to" quaternary size="small" @click="btn.onClick" v-else-if="btn.show !== false">
+      <n-icon>
+        <component :is="iconType[<BtnType>btn.type]" />
+      </n-icon>
+    </Btn>
+  </template>
 </template>
 
 <script setup lang="ts">
-import { DeleteTwotone, EditTwotone, FileTextTwotone } from "@vicons/antd";
+import { DeleteTwotone, EditTwotone, FileAddTwotone, FileTextTwotone } from "@vicons/antd";
 import { arr2Obj } from "@/utils/common.ts";
 
+type BtnType = keyof typeof iconType
 const props = defineProps<{
   buttons: {
     name?: string
-    type: keyof typeof iconType
+    type: BtnType
     to?: string
     id?: number
     onClick?: Function
-    onDelete?:Function
+    onDelete?: Function
+    popConfirm?: boolean | { reload: boolean }
+    show?: boolean
   }[]
 }>();
+
+const _buttons = computed(()=>{
+  return props.buttons
+})
 
 const buttons = arr2Obj(props.buttons, "type");
 const buttonsName = Object.keys(buttons);
@@ -43,15 +51,16 @@ const buttonsName = Object.keys(buttons);
 const iconType = {
   detail: FileTextTwotone,
   edit: EditTwotone,
-  delete: DeleteTwotone
+  delete: DeleteTwotone,
+  add: FileAddTwotone
 };
 
-const message = useMessage()
-const reload = inject('reload') as Function
-const handleDelete = async() =>  {
-  buttons.delete.onDelete && await buttons.delete.onDelete()
-  message.success('已被残忍删除！')
-  reload()
+const message = useMessage();
+const reload = inject("reload") as Function;
+const handleDelete = async () => {
+  buttons.delete.onDelete && await buttons.delete.onDelete();
+  message.success("已被残忍删除！");
+  buttons.delete.popConfirm.reload !== false && reload();
 };
 
 // const tips = {
